a1a522c8140c6f76edad79e9b9cc07377e7b7a3e
Sheyla Murillo <murillocruzsheyla36@gmail.com>
2025-10-25 23:35:35 -0600

    Add activation token to email and redirect to index.html

--- actions/insertDriver.php
+++ actions/insertDriver.php
@@ -1,26 +1,34 @@
 <?php
 
 require_once "../common/Users.php";
+require_once "../actions/Email.php";
 
+$email = new Email();
 $user = new Users();
 
+$firstName = $_POST['first-name'];
+$mail =  $_POST['gmail'];
+
+$token = bin2hex(random_bytes(16));
+
     $resultado = $user->insertUser(
-        $_POST['first-name'],
+        $firstName,
         $_POST['last-name'],
         $_POST['id-card'],
         $_POST['birth-date'],
-        $_POST['gmail'],
+        $mail,
         $_POST['password'],
         $_POST['address'],
         $_POST['phone-number'],
         "Driver",
-        $_FILES['user-photo']
+        $_FILES['user-photo'],
+        $token
     );
 
     echo $resultado === true ? "Usuario registrado correctamente" : $resultado; //ternario
-
-
-
-
-
+    if($resultado){
+        $email->send($mail, $firstName, "Hello $firstName! Activate your account now", "Link: http://shey.web/pages/accountActivation.php?token=$token</b>");
+    }
+    header("Location: ../index.html");
+    exit();
 ?>
