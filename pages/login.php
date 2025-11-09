4a07b4de0ca56173713cff7a57507c7a89dd5e12
Sheyla Murillo <murillocruzsheyla36@gmail.com>
2025-11-08 21:25:13 -0600

    Centered the logo on the public search page and disable clicking on the login

--- pages/login.php
+++ pages/login.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
-    <link rel="stylesheet" href="../styles/index.css">
+    <link rel="stylesheet" href="/styles/index.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
 </head>
 
